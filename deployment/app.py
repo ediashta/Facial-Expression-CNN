96cf6ff7c259490035ded3ffdab15e1ee5c3022e
Your Name <you@example.com>
2023-08-22 15:03:35 +0700

    updated readme.md, updated streamlit app

--- deployment/app.py
+++ deployment/app.py
@@ -1,13 +1,23 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from prediction import predict_emotion
 import eda
 import model_result
-import prediction
-from streamlit_option_menu import option_menu
-
-
 
 st.sidebar.header("Emotion Classification")
 st.title("Facial Emotion Classification")
+
+class EmotionDetectionTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        annotated_frame = predict_emotion(frame)
+        return annotated_frame
+
+def main():
+    st.title('Emotion Detection App')
+    st.write("Press Start")
+
+    webrtc_streamer(key="example", video_transformer_factory=EmotionDetectionTransformer)
 
 with st.sidebar:
     st.write("Ediashta Revindra - FTDS-020")
@@ -29,6 +39,6 @@ if selected == "Distribution":
 elif selected == "Image Sample":
     eda.samples()
 elif selected == "Model Result":
-    model_result.report()   
+    model_result.report()
 elif selected == "Classification":
-    prediction.predict()
+    main()  # Call the main function for emotion detection
--- deployment/main.py
+++ deployment/main.py
@@ -2,53 +2,67 @@ import cv2
 import numpy as np
 from tensorflow.keras.preprocessing import image
 from keras.models import load_model
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load pre-trained emotion classification model
-emotion_classification_model = load_model('.\model\model_fine_tune.h5')  # Replace with actual path
+emotion_classification_model = load_model('deployment\model\model_fine_tune.h5')  # Replace with actual path
 
 # Load Haarcascades face detection classifier
-face_cascade = cv2.CascadeClassifier('.\model\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('deployment\model\haarcascade_frontalface_default.xml')
 
 class_labels = ["angry", "disgusted", "fearful", "happy", "neutral", "sad", "surprised"]
 
-def predict_emotion(frame):
-    img = frame.to_ndarray(format="bgr24")
+# Open webcam
+cap = cv2.VideoCapture(0)
 
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     # Detect faces
     faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
+    
     # Process each detected face
     for (x, y, w, h) in faces:
-        face_img = img[y:y+h, x:x+w]
-
+        face_img = frame[y:y+h, x:x+w]
+        
         # Preprocess the face image
         resized_face_img = cv2.resize(face_img, (48, 48))
         img_array = image.img_to_array(resized_face_img)
         img_array = np.expand_dims(img_array, axis=0)
         img_array /= (255*117)  # Normalize the image
-
+        
         # Perform emotion prediction using the loaded model
         inf_pred_single = emotion_classification_model.predict(img_array)
-
+        
         max_pred_single = np.argsort(inf_pred_single[0])[-2:][::-1]
-
+        
         data_inf_single = []
         rank = []
-
+        
         for i in inf_pred_single[0]:
             value = i * 100
             rank.append(value)
             data_inf_single.append(f'{value.round(2)}%')
-
+        
         rank = (-np.array(rank)).argsort()[:2]
-
+        
         pred_class_single = pd.DataFrame(class_labels).loc[rank][0].tolist()
-
+        
         # Draw bounding box and emotion label on the frame
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.putText(img, f'Emotion: {pred_class_single[0]}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(frame, f'Emotion: {pred_class_single[0]}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    
+    # Display the frame
+    cv2.imshow('Emotion Detection', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    return img  # Return the annotated frame
+# Release webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
